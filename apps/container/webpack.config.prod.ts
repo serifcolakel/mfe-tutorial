829f4290c2a6253ccf5e58ec6dcafebd6cf7c5d7
serifcolakel <serifcolakel0@gmail.com>
2024-03-29 00:24:43 +0300

    Update remote URL for 'info' module for production on netlify

--- apps/container/webpack.config.prod.ts
+++ apps/container/webpack.config.prod.ts
@@ -23,7 +23,7 @@ const prodConfig: ModuleFederationConfig = {
    *   ['app2', 'http://example.com/path/to/app2/remoteEntry.js'],
    * ]
    */
-  remotes: [['info', 'http://localhost:3001/']],
+  remotes: [['info', 'https://animated-lollipop-8d4ee8.netlify.app/']],
 };
 
 // Nx plugins for webpack to build config object from Nx options and context.
