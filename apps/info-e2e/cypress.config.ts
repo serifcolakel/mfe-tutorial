1c2bd8942a7a171efb34bd96cbea5e60aa64141f
Serif <serifcolakel0@gmail.com>
2024-04-06 16:37:24 +0300

    Migrations for nx packages

--- apps/info-e2e/cypress.config.ts
+++ apps/info-e2e/cypress.config.ts
@@ -2,5 +2,6 @@ import { nxE2EPreset } from '@nx/cypress/plugins/cypress-preset';
 import { defineConfig } from 'cypress';
 
 export default defineConfig({
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
   e2e: nxE2EPreset(__dirname),
 });
