6c6cd95e8ae841e785cc8439cf7b4d085b3fd01a
Serif <serifcolakel0@gmail.com>
2024-04-06 16:58:56 +0300

    Update Cypress configuration in info and container apps

--- apps/container-e2e/cypress.config.ts
+++ apps/container-e2e/cypress.config.ts
@@ -2,6 +2,5 @@ import { nxE2EPreset } from '@nx/cypress/plugins/cypress-preset';
 import { defineConfig } from 'cypress';
 
 export default defineConfig({
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
   e2e: nxE2EPreset(__dirname),
 });
